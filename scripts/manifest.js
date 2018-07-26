134e9713711c190f44e5ac43dc4c4c3fd9f76dc5
Harry Tormey <harrytormey@Harrys-MacBook-Pro.local>
2018-07-25 19:40:13 -0700

    Refactor to read config files

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -4,27 +4,37 @@
 var fs = require('fs');
 var path = require('path');
 
-console.log("Build manifest from files on disk");
-
 try {
     const screenPath = path.join(__dirname, '..', 'src', 'screens');
     const iosPath = path.join(__dirname, '..', 'ios', 'BrownFieldExample', 'screens.json');
     fs.readdir(screenPath, function(err, items) {
-        const skipFiles = ['NotFoundScreen'];
         let screens = {}
-        if (items && items.length > 0) {
-            items.forEach( (item) => {
-                if (skipFiles.indexOf(item) == -1) {
-                    console.log(item);
-                    screens[item] = {}
-                    screens[item]['screen'] = item               
-                    screens[item]['config'] = item + 'Config'
-                }
-            })
+        //'screens' : screenConfigs
+        let addProperties = (item, properties, lastFile) => {
+            screens[item]['properties'] = properties
+            if (lastFile) {
+                console.log('Screens config file is', screens);
+                fs.writeFileSync(iosPath, JSON.stringify(screens), 'utf8');
+            }
         }
-        console.log('Screens config file is', screens);
-        fs.writeFileSync(iosPath, JSON.stringify(screens), 'utf8')            
-        
+        items = items.filter(item => item !== 'NotFoundScreen');
+        if (items && items.length > 0) {
+            items.forEach( (item, index) => {
+                console.log('Index is ', index)
+                console.log(item);
+                screens[item] = {};
+                screens[item]['name'] = item;
+                const configPath = path.join(__dirname, '..', 'src', 'screens', item, 'config.json');
+                fs.readFile(configPath, 'utf8', function (err, data) {
+                    if (err) {
+                      return console.log(err);
+                    }
+                    console.log('Adding data', data);
+                    console.log('length is ', items.length);
+                    addProperties(item, data, index === (items.length-1));
+                  });
+            });
+        }
     });
 } catch(error) {
     console.log('Error reading screens directory ', error);
