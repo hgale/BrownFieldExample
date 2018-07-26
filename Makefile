21a7b47087df3246925b918a9f8738619dc59384
Harry Tormey <harrytormey@Harrys-MacBook-Pro.local>
2018-07-26 16:12:27 -0700

    Generate screens.json

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 RN_DIR=./
 
+SCRIPTS_DIR=./scripts
+
 react-native-developer-mode:
 	cd '$(RN_DIR)/node_modules/react-native'; npm run start — — root $(RN_DIR)
 
 react-native-bundle: 
-	cd $(RN_DIR); react-native bundle --dev false --platform ios --entry-file index.js --bundle-output ./ios/BrownFieldExample/main.jsbundle --assets-dest ./	
+	cd $(RN_DIR); react-native bundle --dev false --platform ios --entry-file index.js --bundle-output ./ios/BrownFieldExample/main.jsbundle --assets-dest ./
+
+build-screens-file:
+	cd $(SCRIPTS_DIR); node screens.js
--- scripts/screens.js
+++ scripts/screens.js
@@ -4,25 +4,25 @@
 var fs = require('fs');
 var path = require('path');
 
+console.log('Attempting to build list of screens from src/screens');
+
 try {
     const screenPath = path.join(__dirname, '..', 'src', 'screens');
     const iosPath = path.join(__dirname, '..', 'ios', 'BrownFieldExample', 'screens.json');
     fs.readdir(screenPath, function(err, items) {
         let screens = []
-        //'screens' : screenConfigs
         let addProperties = (config, properties, lastFile) => {
             config['properties'] = JSON.parse(properties)
             screens.push(config)
             if (lastFile) {
-                console.log('Screens config file is', screens);
                 fs.writeFileSync(iosPath, JSON.stringify({'screens':screens}), 'utf8');
+                console.log('New screens.json generated.');
+                
             }
         }
         items = items.filter(item => item !== 'NotFoundScreen');
         if (items && items.length > 0) {
             items.forEach( (item, index) => {
-                console.log('Index is ', index)
-                console.log(item);
                 let config = {}
                 config['name'] = item;
                 const configPath = path.join(__dirname, '..', 'src', 'screens', item, 'config.json');
@@ -30,8 +30,6 @@ try {
                     if (err) {
                       return console.log(err);
                     }
-                    console.log('Adding data', data);
-                    console.log('length is ', items.length);
                     addProperties(config, data, index === (items.length-1));
                   });
             });
